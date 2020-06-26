280353779370bc6b7d460636f57c212094c9215e
grenoshka <stoiodinkot@gmail.com>
2020-06-27 05:31:18 +0700

    диалог
    диалог

--- Assets/Scripts/Reality/StartDialog.cs
+++ Assets/Scripts/Reality/StartDialog.cs
@@ -6,13 +6,10 @@ public class StartDialog : MonoBehaviour
 {
     public int n;
     public GameObject hint;
-    public GameObject inputField;
-    public GameObject[] StartReplics = new GameObject[2];
-    public GameObject[] MotherAnswer = new GameObject[8];
-
-
+    public GameObject[] dialogs = new GameObject[10];
+    public GameObject[] secDialog = new GameObject[3];
     private GameObject Character;
-    private bool flag = false, motherTalking = false, playerTalking = true, dialogEnded = false;
+    private bool flag = false, dialogEnded = false, mainEnded = false;
     private Animator anim;
     private int i = 0;
 
@@ -20,16 +17,55 @@ public class StartDialog : MonoBehaviour
     void Start()
     {
         Character = GameObject.FindGameObjectWithTag("Player");
-        inputField.SetActive(false);
         for (int j = 0; j < n; j++)
-            MotherAnswer[j].SetActive(false);
+        {
+            if (j<3)
+                secDialog[j].SetActive(false);
+            dialogs[j].SetActive(false);
+        }
         anim = GetComponent<Animator>();
         hint.SetActive(false);
 
     }
     private void Update()
     {
-        
+        if (Input.GetKeyDown(KeyCode.E))
+        {
+            if (flag)
+            {
+                if (!mainEnded)
+                {
+                    dialogs[i].SetActive(false);
+                    i++;
+                    if (i < n)
+                        dialogs[i].SetActive(true);
+                    else
+                    {
+                        i = 0;
+                        flag = false;
+                        Character.GetComponent<CharacterControl>().isInDialog = false;
+                        anim.SetBool("isTalking", false);
+                        dialogEnded = true;
+                        mainEnded = true;
+                    }
+                }
+                else
+                {
+                    secDialog[i].SetActive(false);
+                    i++;
+                    if (i < 3)
+                        secDialog[i].SetActive(true);
+                    else
+                    {
+                        i = 0;
+                        flag = false;
+                        Character.GetComponent<CharacterControl>().isInDialog = false;
+                        anim.SetBool("isTalking", false);
+                        dialogEnded = true;
+                    }
+                }
+            }
+        }
     }
     private void OnTriggerEnter2D(Collider2D collision)
     {
@@ -48,15 +84,17 @@ public class StartDialog : MonoBehaviour
             {
                 if (!flag)
                 {
-                    i = -1;
+                    i = 0;
                     flag = true;
                     Character.GetComponent<CharacterControl>().isInDialog = true;
                     Character.GetComponent<CharacterAnimation>().anim.SetBool("isWalking", false);
                     if (!Character.GetComponent<CharacterControl>().facingRight)
                         Character.GetComponent<CharacterControl>().Flip();
-                    inputField.transform.position = new Vector3(Character.transform.position.x + 2.2f, Character.transform.position.y + 3.4f, 0);
                     anim.SetBool("isTalking", true);
-                    playerTalking = true;
+                    if (!mainEnded)
+                        dialogs[i].SetActive(true);
+                    else
+                        secDialog[i].SetActive(true);
                     //MotherAnswer[i].SetActive(true);
                     //playerTalking = true;
                     hint.SetActive(false);
@@ -68,13 +106,90 @@ public class StartDialog : MonoBehaviour
     {
         if (collision.gameObject.tag == "Player" && !dialogEnded)
         {
+            dialogEnded = false;
             hint.SetActive(false);
-            //     dialogField.SetActive(false);
-            //      text.SetActive(false);
             flag = false;
         }
     }
 }
+
+//using System.Collections;
+//using System.Collections.Generic;
+//using UnityEngine;
+
+//public class StartDialog : MonoBehaviour
+//{
+//    public int n;
+//    public GameObject hint;
+//    public GameObject inputField;
+//    public GameObject[] StartReplics = new GameObject[2];
+//    public GameObject[] MotherAnswer = new GameObject[8];
+
+
+//    private GameObject Character;
+//    private bool flag = false, motherTalking = false, playerTalking = true, dialogEnded = false;
+//    private Animator anim;
+//    private int i = 0;
+
+
+//    void Start()
+//    {
+//        Character = GameObject.FindGameObjectWithTag("Player");
+//        inputField.SetActive(false);
+//        for (int j = 0; j < n; j++)
+//            MotherAnswer[j].SetActive(false);
+//        anim = GetComponent<Animator>();
+//        hint.SetActive(false);
+
+//    }
+//    private void Update()
+//    {
+
+//    }
+//    private void OnTriggerEnter2D(Collider2D collision)
+//    {
+//        if (collision.gameObject.tag == "Player")
+//        {
+//            dialogEnded = false;
+//            hint.SetActive(true);
+//        }
+//    }
+
+//    private void OnTriggerStay2D(Collider2D collision)
+//    {
+//        if (collision.gameObject.tag == "Player" && !dialogEnded)
+//        {
+//            if (Input.GetKeyDown(KeyCode.E))
+//            {
+//                if (!flag)
+//                {
+//                    i = -1;
+//                    flag = true;
+//                    Character.GetComponent<CharacterControl>().isInDialog = true;
+//                    Character.GetComponent<CharacterAnimation>().anim.SetBool("isWalking", false);
+//                    if (!Character.GetComponent<CharacterControl>().facingRight)
+//                        Character.GetComponent<CharacterControl>().Flip();
+//                    inputField.transform.position = new Vector3(Character.transform.position.x + 2.2f, Character.transform.position.y + 3.4f, 0);
+//                    anim.SetBool("isTalking", true);
+//                    playerTalking = true;
+//                    //MotherAnswer[i].SetActive(true);
+//                    //playerTalking = true;
+//                    hint.SetActive(false);
+//                }
+//            }
+//        }
+//    }
+//    private void OnTriggerExit2D(Collider2D collision)
+//    {
+//        if (collision.gameObject.tag == "Player" && !dialogEnded)
+//        {
+//            hint.SetActive(false);
+//            //     dialogField.SetActive(false);
+//            //      text.SetActive(false);
+//            flag = false;
+//        }
+//    }
+//}
 
 //if (Input.GetKeyDown(KeyCode.E) && flag)
 //        {
